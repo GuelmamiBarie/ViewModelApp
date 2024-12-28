36e6f227c9fac10390c8d80ace48ec7708ca5777
Barie <guelmami_abdelbari@hotmail.fr>
2024-12-28 21:04:03 +0100

    1st update

--- app/src/main/java/vistula/glmbr/viewmodelapp/MainActivity.java
+++ app/src/main/java/vistula/glmbr/viewmodelapp/MainActivity.java
@@ -7,18 +7,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import vistula.glmbr.viewmodelapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    myviewmodel viewModel;
+    ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        mainBinding = DataBindingUtil.setContentView(this,
+                R.layout.activity_main);
+
+
+        viewModel = new ViewModelProvider(this)
+                .get(myviewmodel.class);
+
+        // Link the Databinding with ViewModel
+        mainBinding.setMyviewmodel(viewModel);
+
+        // Observing the LiveData
+        viewModel.getCounter().observe(this,
+                new Observer<Integer>() {
+                    @Override
+                    public void onChanged(Integer counter) {
+                        // Update the UI when the LiveData Changes
+                        mainBinding.textView2.setText(""+counter);
+                    }
+                });
+
+
     }
 }
